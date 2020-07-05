cc1bb477c109c2ac75d02bd1f7e4152fe3f4838b
MohamadMilhem <mohamad1001600@outlook.com>
2020-07-05 22:55:27 +0300

    styling the Header and fix Omar's picture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,39 +8,50 @@ import { makeStyles } from "@material-ui/core/styles";
 import logo from "../logo.png";
 import Link from "react-router-dom/Link";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  Typography: {
+    color: "#61DAFB",
+  },
+  links: {
+    textDecoration: "none",
+  },
+  AppBar: {
+    background: "#2E3B55",
+    position: "static",
+  },
+}));
 
 const Header = () => {
-  const classes = useStyles();
+  const Classes = useStyles();
   return (
-    <div className={classes.container}>
-      <AppBar position="static" color="inherit">
+    <div className={Classes.container}>
+      <AppBar className={Classes.AppBar}>
         <Toolbar>
-          <Link to="/">
+          <Link to="/" className={Classes.links}>
             <img src={logo} alt="logo" className="logo" />
           </Link>
-          <Link to="/Compare">
+          <Link to="/Compare" className={Classes.links}>
             <IconButton>
-              <CompareIcon />
-              <Typography>Compare</Typography>
+              <CompareIcon className={Classes.Typography} />
+              <Typography className={Classes.Typography}>Compare</Typography>
             </IconButton>
           </Link>
-          <Link to="/Search">
+          <Link to="/Search" className={Classes.links}>
             <IconButton>
-              <SearchIcon />
-              <Typography>Search</Typography>
+              <SearchIcon className={Classes.Typography} />
+              <Typography className={Classes.Typography}>Search</Typography>
             </IconButton>
           </Link>
-          <Link to="/About">
+          <Link to="/About" className={Classes.links}>
             <IconButton>
-              <InfoIcon />
-              <Typography>About</Typography>
+              <InfoIcon className={Classes.Typography} />
+              <Typography className={Classes.Typography}>About</Typography>
             </IconButton>
           </Link>
-          <Link to="/Feedback">
+          <Link to="/Feedback" className={Classes.links}>
             <IconButton>
-              <FeedbackIcon />
-              <Typography>Feedback</Typography>
+              <FeedbackIcon className={Classes.Typography} />
+              <Typography className={Classes.Typography}>Feedback</Typography>
             </IconButton>
           </Link>
         </Toolbar>
