95ea77b7828893b9b282e4a58479a5300746d6e2
Sireen Qaddah <sireen.qaddah@gmail.com>
2020-07-10 18:51:18 +0300

    searchPage changei

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Checkboxes from "./Checkboxes.js";
 const Search = () => {
   return (
     <div>
-      <h1>this is Search</h1>
+      <h1>Search Page</h1>
+      <Checkboxes />
     </div>
   );
 };
