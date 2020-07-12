ae4eca5557823aaf407cddf5ff1e1054d2f38de9
MohamadMilhem <mohamad1001600@outlook.com>
2020-07-12 20:22:31 +0300

    Footer and new logo

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -6,29 +6,49 @@ import Home from "./Home.jsx";
 import Feedback from "./Feedback.jsx";
 import Compare from "./Compare.jsx";
 import Search from "./Search.jsx";
+import Footer from "./Footer.jsx";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  pageContainer: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "220vh",
+  },
+
+  contentWrap: {
+    flex: 1,
+  },
+}));
 
 const MainPage = () => {
+  const Classes = useStyles();
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/About">
-          <About />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/Search">
-          <Search />
-        </Route>
-        <Route exact path="/Compare">
-          <Compare />
-        </Route>
-        <Route exact path="/Feedback">
-          <Feedback />
-        </Route>
-      </Switch>
-    </Router>
+    <div className={Classes.pageContainer}>
+      <Router>
+        <div className={Classes.contentWrap}>
+          <Header />
+          <Switch>
+            <Route path="/About">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/Search">
+              <Search />
+            </Route>
+            <Route exact path="/Compare">
+              <Compare />
+            </Route>
+            <Route exact path="/Feedback">
+              <Feedback />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   );
 };
 
