b79ea3af53d6db85dd430cdf2a6d3b9040e2ba86
Sireen Qaddah <sireen.qaddah@gmail.com>
2020-07-15 11:23:44 +0300

    changing searchPage photo

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,10 +5,13 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   img: {
-    width: "100%",
+    width: "70%",
+    height: "70%",
+    marginTop: 0,
     postion: "relative",
     backgroundImage: "url(" + { Searchphoto } + ")",
     marginTop: 0,
+    marginLeft: 400,
   },
   text: {
     textAlign: "center",
